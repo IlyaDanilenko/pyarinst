14d4af56320f0659b9e11290122eb2b1beb68ba4
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2022-06-17 13:23:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version='1.0',
     description='Python SDK for Arinst devices',
     author='Ilya Danilenko',
-    author_email='ilya@eadsoft.com',
     packages=['pyarinst'],
     install_requires=['pyserial'],
     long_description='Python SDK for Arinst devices'
