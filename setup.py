0d70b3daaa436a01ef29c47f604f117d04b1c49a
Ilya Danilenko <47718218+IlyaDanilenko@users.noreply.github.com>
2023-05-09 18:53:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyarinst',
-    version='1.0.0',
+    version='1.0.1',
     description='Python SDK for Arinst devices',
     author='Ilya Danilenko',
     packages=['pyarinst'],
